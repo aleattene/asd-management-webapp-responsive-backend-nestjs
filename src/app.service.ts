b618442cb758e9b504c51bf1ef14e580ffc2994d
aleattene <aleattene@gmail.com>
2025-03-11 13:12:48 +0100

    chore: add version to base-api

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'ASD Management - Deploy NestJS works successfully!';
-  }
+	getHello(): string {
+		return 'ASD Management - Deploy NestJS works successfully!';
+	}
 }
